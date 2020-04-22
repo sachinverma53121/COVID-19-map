1ab4060aee0d83c74b43d7cd75c11033a2074e8d
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-04-22 22:39:04 +0530

    ready with a demo data

--- index.js
+++ index.js
@@ -11,10 +11,30 @@ function updateMap() {
         longitude = element.longitude;
         //console.log(latitude + " " + longitude);
 
+        // generate colour based on no. of cases
+        cases = element.infected;
+        if (cases < 100) color = "rgb(201,102,85)";
+        else if (cases < 200) color = "rgb(250,128,114)";
+        else if (cases < 300) color = "rgb(240,128,128)";
+        else if (cases < 400) color = "rgb(205,92,92)";
+        else if (cases < 500) color = "rgb(255,99,71)";
+        else if (cases < 750) color = "rgb(220,20,60)";
+        else if (cases < 1000) color = "rgb(178,34,34)";
+        else if (cases < 2000) color = "rgb(165,42,42)";
+        else if (cases < 4000) color = "rgb(128,0,0)";
+        else color = "rgb(77,11,0)";
+
         // Mark on map
+        new mapboxgl.Marker({
+          draggable: false,
+          color: color,
+        })
+          .setLngLat([longitude, latitude])
+          .addTo(map);
       });
     })
     .catch();
 }
-
 updateMap();
+// updatemap in  120 sec
+setInterval(updateMap, 120000);
